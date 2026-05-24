15dbdcdd84bda47137fa48de6b468b2c7131b57c
TruthExperience <amwade537@gmail.com>
2026-05-24 15:05:25 -0400

    Create index.ts

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("PitBossOS API Running");
+});
+
+app.listen(4000, () => {
+  console.log("API running on http://localhost:4000");
+});
