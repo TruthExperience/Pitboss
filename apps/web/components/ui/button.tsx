124b477c8d72c5d8d0c95cded8bc0e9e7cc8b208
TruthExperience <amwade537@gmail.com>
2026-05-24 15:50:17 -0400

    Create button.tsx

---
+++ apps/web/components/ui/button.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => (
+    <button
+      ref={ref}
+      className={cn(
+        "px-4 py-2 rounded bg-neutral-800 hover:bg-neutral-700 text-white",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+
+Button.displayName = "Button";
